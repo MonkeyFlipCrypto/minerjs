75620f5cd0f5432b78f4fec47440e6b781948854
Tristan Ross <rosscomputerguy@protonmail.com>
2021-04-15 15:08:04 -0700

    Added latest changes

--- src/miner.ts
+++ src/miner.ts
@@ -2,6 +2,7 @@ import Block from './block'
 import { MinerConfig, defaultConfig } from './config'
 import got from 'got'
 import { EventEmitter } from 'events'
+import random from 'random'
 
 interface RequestConfig {
 	auth?: boolean
@@ -25,7 +26,9 @@ const REQ_MAP: Record<string, RequestConfig> = {
 interface ResponseInfo {
 	freeBlocks: number
 	totalBlocks: number
-	ownerBlocks: number
+	ownedBlocks: number
+	difficulty: number
+	lastClaimedTimestamp: string
 	firstBlock: {
 		index: number
 		hash: string
@@ -37,6 +40,16 @@ interface MiningOptions {
 	count?: number
 	startDate?: Date
 	endDate?: Date
+}
+
+export interface MiningResults {
+	found: number
+	failed: number
+	total: number
+	startDate: Date
+	endDate: Date
+	totalTime: number
+	foundBlocks: Block[]
 }
 
 export default class Miner extends EventEmitter {
@@ -57,37 +70,69 @@ export default class Miner extends EventEmitter {
 		return this.blockchain[this.blockchain.length - 1]
 	}
 
-	async mine(options: MiningOptions = {
-			count: 1,
-			startDate: new Date()
-		}): Promise<number> {
-		if (!options.startDate) options.startDate = new Date()
-		if (!options.endDate) options.endDate = new Date(options.startDate.getTime() + options.count)
-
-		let currTime = options.startDate.getTime()
-		const incTime = (options.endDate.getTime() - options.startDate.getTime()) / options.count
+	async mine(count: number = 1): Promise<MiningResults> {
 		let owns = 0
+		let info: ResponseInfo = await this.request('/info')
+		if (this.diff !== info.difficulty) this.diff = info.difficulty
 
-		while (options.count > 0) {
-			const block = this.addBlock(new Date(currTime))
-			currTime += incTime
-			options.count--;
+		const startDate = Date.now()
+		const foundBlocks: Block[] = []
+		const startCount = count
 
-			if (await block.verify()) {
-				this.emit('block:found', block)
-				owns++;
+		while (count > 0) {
+			info = await this.request('/info')
+			let timestamp = new Date(new Date(info.lastClaimedTimestamp).toLocaleString('en-US', { timeZone: 'UTC' })).getTime()
+			const range = [this.diff, this.diff + Math.pow(info.totalBlocks, 2)]
+			const rands = []
+			let found = false
+
+			while (found == false) {
+				let rand = random.int(range[0], range[1])
+				while (rands.indexOf(rand) == -1) rand = random.int(range[0], range[1])
+				rands.push(rand)
+				const block = this.addBlock(new Date(timestamp + rand))
+
+				if (await block.verify()) {
+					this.emit('block:found', block)
+					owns++;
+					found = true
+					foundBlocks.push(block)
+				} else {
+					const index = this.blockchain.indexOf(block)
+					if (index === -1) throw new Error('Failed to pop block, invalid index')
+					this.blockchain.splice(index, 1)
+				}
 			}
+			count--;
 		}
 
-		return owns
+		const endDate = Date.now()
+		const totalTime = endDate - startDate
+
+		const seqInfo: MiningResults = {
+			found: foundBlocks.length,
+			failed: foundBlocks.length - startCount,
+			total: startCount,
+			startDate: new Date(startDate),
+			endDate: new Date(endDate),
+			totalTime,
+			foundBlocks
+		}
+		this.emit('mining:sequence', seqInfo)
+		return seqInfo
 	}
 
 	async init(): Promise<void> {
 		const resp: ResponseInfo = await this.request('/info')
+
+		this.blockchain = []
+
 		const block = new Block(this, resp.firstBlock.index, new Date(resp.firstBlock.timestamp), 'Initial block in chain', '0')
 		block.hash = block.computeHash()
 		if (block.hash !== resp.firstBlock.hash) throw new Error('Failed to recreate genesis block')
 		this.blockchain.push(block)
+
+		this.diff = resp.difficulty
 	}
 
 	addBlock(date: Date = new Date()): Block {
